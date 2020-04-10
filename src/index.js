0106f0a4f4bbe52242a5d36fe73744f8469d5a4c
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 15:45:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-fetch("https://dog.ceo/api/breeds/image/random/4")
+function challenge1(){
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .this(function(response){
+    response.json
+  })
+  .this(function(json)){
+    console.log(json)
+  }
+} 
 
 document.addEventListener("DOMContentLoaded", () => {
-
+  
 });
