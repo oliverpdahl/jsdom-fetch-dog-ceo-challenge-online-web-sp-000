89b3788e4cc24390e74e6dcf875a628e85053c5b
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 16:54:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function challenge2(){
 
 function challenge3(){
   const li = document.getElementsByClassName('dog-breed-li');
-  console.log(li)
+  console.log(li.first)
   console.log(li[5])
   li.addEventListener('click', function(event){
     console.log('clicked')
