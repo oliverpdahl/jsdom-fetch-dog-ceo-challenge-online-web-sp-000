f1fd6cb23c14d927204846241319206955460137
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 17:02:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ function challenge3(){
   let li = document.getElementById('super-breed')
   li.addEventListener('click', function(event){
     console.log('clicked')
-    li.style.color = "red";
+    li.innerText.style.color = "red";
   })
 }
 
